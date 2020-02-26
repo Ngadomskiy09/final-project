b236a0df707f9c989212b1d4a0e1f3cc21165ed3
Nick Gadomskiy <ngadomskiy@gmail.com>
2020-02-26 14:53:25 -0500

    Changed home page to link to create page

--- index.php
+++ index.php
@@ -11,15 +11,20 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Require the autoload file
-require_once('vendor/autoload.php');
+require('vendor/autoload.php');
 
 // Create an instance of the Base class
 $f3 = Base::instance();
 
 // Define a default route
-$f3->route('GET/', function() {
-    $views = new Template();
-    echo $views->render("views/home.html");
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render("views/home.html");
+});
+
+$f3->route('GET /create', function() {
+    $view = new Template();
+    echo $view->render("views/create.html");
 });
 
 // Run fat free
